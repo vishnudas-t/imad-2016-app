e385cbf0fc31c023c81e1a99f2d169d2160687ca
vishnudas-t <tvishnudas123@gmail.com>
2016-11-21 18:00:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,9 +27,7 @@ app.post('/login',function(req,res){
   console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
 });
-app.listen(3000,function(){
-  console.log("Started on PORT 3000");
-})
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
